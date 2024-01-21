b5b252c9c6ccd4c6499c9e588d3ece440f531bdf
Mahomedalid Aguirre <mahomedalidla@gmail.com>
2024-01-21 15:23:11 -0700

    Update hello.flow.ts
    se esta trabajando el un menu princiapl para el usuario

--- src/flow/hello.flow.ts
+++ src/flow/hello.flow.ts
@@ -1,12 +1,41 @@
 import BotWhatsapp from '@bot-whatsapp/bot';
+import paypalFlow from './paypal.flow';
+import welcomeFlow from './welcome.flow';
+
+const handleMenuSelection = (ctx: any, { state, fallBack }: any) => {
+    const selection = ctx.body;
+    switch (selection) {
+      case '1':
+        // Acción para la opción 1
+        fallBack('Has seleccionado la opción 1. que es ir a pago con paypal.');
+        BotWhatsapp.createFlow(
+            [
+                paypalFlow
+            ]
+        )
+        break;
+      case '2':
+        // Acción para la opción 2
+        fallBack('Has seleccionado la opción 2. Aquí va la acción para la opción 2.');
+        BotWhatsapp.createFlow(
+            [
+                welcomeFlow
+            ]
+        )
+        break;
+      // Añade más casos para más opciones del menú
+      default:
+        fallBack('Opción no válida. Por favor, selecciona una opción del menú.');
+        break;
+    }
+  };
 
 /**
- * Un flujo conversacion que responder a las palabras claves "hola", "buenas", ...
+ * Un flujo de conversación que responde a las palabras clave "hola", "buenas", ...
  */
 export default BotWhatsapp.addKeyword(['hola', 'buenas'])
-    .addAnswer('Flow hellow ------- Un gusto tenerte de nuevo ¿Como puedo ayudarte el día de hoy 😀?')
-
-
+  .addAnswer('Flow hello ------- Un gusto tenerte de nuevo ¿Como puedo ayudarte el día de hoy 😀?')
+  .addAnswer('Por favor, selecciona una opción del menú:\n1. Opción 1, paypal\n2. Opción 2 chatea con nuestro asesor', { capture: true }, handleMenuSelection);
 
 //     import BotWhatsapp from '@bot-whatsapp/bot';
 
